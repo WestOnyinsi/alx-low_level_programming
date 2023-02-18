e93c8d12a115aa8099a6c7d1c8e14277d00c0328
West <westonyinsi.com>
2023-02-18 10:39:11 -0800

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- 
-  * main - Prints onto screen
-  *
-  * Return: zero
-  */
+ * main - Use the function printf
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
