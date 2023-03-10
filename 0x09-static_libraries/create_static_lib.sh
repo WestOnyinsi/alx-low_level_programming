0dc939d38f3fa2cb1f9f64a9ada8bd28cd0c7525
West <westonyinsi.com>
2023-03-10 11:37:36 -0800

    bleh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Find all .c files in the current directory
-c_files=$(find . -name "*.c")
-
-# Compile the .c files into object files
-for file in $c_files
-do
-    gcc -c $file
-done
-
-# Create the static library from the object files
-ar rcs liball.a *.o
-
-# Remove the object files
-rm *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
