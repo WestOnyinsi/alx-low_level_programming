a8302257031bfd39429296d381738fd29fbcdd96
West <westonyinsi.com>
2023-04-14 12:23:25 -0700

    tuone tena

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,57 +1,91 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fdcdtdftff);
 
 /**
- * Copies the contents of the file at the specified path to another file at the
- * specified path.
- *
- * @param fromPath The path of the file to copy from.
- * @param toPath The path of the file to copy to.
- * @return 0 if the operation was successful, or a positive integer representing
- * the error code if an error occurred.
+ * @file:hifuhuih 6bh6jb6b6b bb4bb664765b4 yvu
  */
-int copyFile(const char *fromPath, const char *toPath) {
-    // Open the file to copy from.
-    int fromFd = open(fromPath, O_RDONLY);
-    if (fromFd < 0) {
-        return 1;
-    }
+char *create_buffer(char *file)
+{
+	char *buffer;
 
-    // Open or create the file to copy to.
-    int toFd = open(toPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (toFd < 0) {
-        close(fromFd);
-        return 2;
-    }
+	buffer = malloc(sizeof(char) * 1024);
 
-    // Copy the file's contents in chunks of BUFFER_SIZE bytes.
-    char *buffer = (char *) malloc(BUFFER_SIZE);
-    ssize_t bytesRead, bytesWritten;
-    while ((bytesRead = read(fromFd, buffer, BUFFER_SIZE)) > 0) {
-        bytesWritten = write(toFd, buffer, bytesRead);
-        if (bytesWritten < 0) {
-            free(buffer);
-            close(fromFd);
-            close(toFd);
-            return 3;
-        }
-    }
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
 
-    // Check for read errors.
-    if (bytesRead < 0) {
-        free(buffer);
-        close(fromFd);
-        close(toFd);
-        return 4;
-    }
+	return (buffer);
+}
 
-    // Clean up and exit successfully.
-    free(buffer);
-    close(fromFd);
-    close(toFd);
-    return 0;
+/**
+ * @fdcdtdftff: bdvhsyg7 78y78  7t7 t 7g u7  r   u d
+ */
+void close_file(int fdcdtdftff)
+{
+	int c;
+
+	c = close(fdcdtdftff);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fdcdtdftff %d\n", fdcdtdftff);
+		exit(100);
+	}
+}
+
+/**
+ * @argc: cgf78ct7teg7t8t3v v78rg87 8tr7e v6e 7rv6cte76co7t37o7t3o7t t fgf7 6dtocgd7g
+ * @argv: dgcfuyn7tx67t667 6rb  br6r68     e5 e e5 b e e 5 8edyf fvv h v v 
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
 }
